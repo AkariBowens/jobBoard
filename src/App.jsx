b0c63578adff493a64012cdbe1788d067f57696c
Akari Bowens <akarimbowens@gmail.com>
2023-07-11 18:42:21 -0700

    Adding Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="App flex">
+      <div className="header__container w-screen h-12 mb-5 px-8 flex flex-row items-center justify-between ">
+        <div className="header__left flex justify-between ">
+          <a href="">Jobs</a>
+          <a href="">resume</a>
+        </div>
+        <div className="header__center flex">
+          <a href="">Logo + Home</a>
+        </div>
+        <div className="header__right flex justify-between ">
+          <a href="">Search</a>
+          <a href="">Avatar</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;

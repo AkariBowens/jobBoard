b2c23f1495304057d2b98e483639cb30dd3702c2
Akari Bowens <akarimbowens@gmail.com>
2023-07-13 15:52:06 -0700

    Adding hover pseudo-class to Header elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,50 @@
+import {
+  AccountCircleOutlined,
+  DocumentScannerOutlined,
+  SearchOutlined,
+  WorkOutlineOutlined,
+} from "@mui/icons-material";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App flex">
-      <div className="header__container w-screen h-12 mb-5 px-8 flex flex-row items-center justify-between ">
-        <div className="header__left flex justify-evenly w-1/4">
-          <a href="">Jobs</a>
-          <a href="">resume</a>
+    <div className="App flex justify-center">
+      {/* Header */}
+
+      <div className="header__container w-screen h-12 mt-4 mb-5  flex flex-row items-center justify-evenly ">
+        <div className="header__left flex justify-between mx-4 min-w-32 max-w-1/4">
+          <a href="" className="m-8 ">
+            <WorkOutlineOutlined
+              fontSize="large"
+              className="hover:fill-slate-200"
+            />
+          </a>
+          <a href="" className="m-8">
+            <DocumentScannerOutlined
+              fontSize="large"
+              className="hover:fill-slate-200"
+            />
+          </a>
         </div>
-        <div className="header__center flex justify-evenly m-auto">
-          <a href="">Logo + Home</a>
+        <div className="header__center flex text-3xl font-bold justify-evenly w-1/2 max-w-1/2 basis-2 mx-4 px-2">
+          <a href="" className="m-8">
+            Home
+          </a>
         </div>
-        <div className="header__right flex justify-evenly w-1/4">
-          <a href="">Search</a>
-          <a href="">Avatar</a>
+        <div className="header__right flex justify-between mx-4 min-w-32 max-w-1/4">
+          <a href="" className="m-8">
+            <SearchOutlined fontSize="large" className="hover:fill-slate-200" />
+          </a>
+          <a href="" className="m-8">
+            <AccountCircleOutlined
+              fontSize="large"
+              className="hover:fill-slate-200"
+            />
+          </a>
         </div>
       </div>
+
+      {/* Landing */}
     </div>
   );
 }

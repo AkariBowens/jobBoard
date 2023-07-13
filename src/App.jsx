61c41e7a6cfa851a307a53028f41e4cde92f5422
Akari Bowens <akarimbowens@gmail.com>
2023-07-12 17:08:29 -0700

    Adding margin and spacing to header

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ function App() {
   return (
     <div className="App flex">
       <div className="header__container w-screen h-12 mb-5 px-8 flex flex-row items-center justify-between ">
-        <div className="header__left flex justify-between ">
+        <div className="header__left flex justify-evenly w-1/4">
           <a href="">Jobs</a>
           <a href="">resume</a>
         </div>
-        <div className="header__center flex">
+        <div className="header__center flex justify-evenly m-auto">
           <a href="">Logo + Home</a>
         </div>
-        <div className="header__right flex justify-between ">
+        <div className="header__right flex justify-evenly w-1/4">
           <a href="">Search</a>
           <a href="">Avatar</a>
         </div>
